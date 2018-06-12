017e0e98a3587d042796ede12d9cd217cf9b7818
Polina Tumanian <>
2018-06-12 22:42:16 +0300

    updated 1

---
+++ task3.js
@@ -0,0 +1,23 @@
+// Write a function splitAndMerge
+
+// Function accept 2 parameters:str and sp. str is a sentence. sp is a char as separator. 
+// First we need to divide the sentence into words(Use separator space); 
+// and then divide each word into characters(Use separator empty string); 
+// and then merge each characters with the specified sp; at last merge all the words(Use separator space) and return it.
+
+// Example:
+
+// splitAndMerge("My name is John"," ") should return "M y n a m e i s J o h n"
+// splitAndMerge("Hello World!",",") should return "H,e,l,l,o W,o,r,l,d,!"
+
+function splitAndMerge(str, sp) {
+    let a = str.split(' ')
+        .map(str => str.split(''));
+    let arr = [];
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a[i].length; j++) {
+            arr.push(a[i][j]);
+        }
+    }
+    return arr.join(sp);
+}
